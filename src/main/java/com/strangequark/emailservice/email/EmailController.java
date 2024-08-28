4b35652f6f3661ca76843326638b9549d7a88cb7
StrangeQuark <zacharyzirkle@hotmail.com>
2024-08-28 15:45:11 -0500

    Add /email prefix to controller

--- src/main/java/com/strangequark/emailservice/email/EmailController.java
+++ src/main/java/com/strangequark/emailservice/email/EmailController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/email")
 @CrossOrigin
 public class EmailController {
     private final EmailService emailService;
@@ -12,27 +13,27 @@ public class EmailController {
         this.emailService = emailService;
     }
 
-    @PostMapping(path = "sendEmail")
+    @PostMapping(path = "/sendEmail")
     public ResponseEntity<?> sendEmail(@RequestBody EmailRequest request) {
         return emailService.send(request.getRecipient(), request.getSender(), request.getEmail(), request.getSubject());
     }
 
-    @PostMapping(path = "sendEmailWithToken")
+    @PostMapping(path = "/sendEmailWithToken")
     public ResponseEntity<?> sendEmailWithToken(@RequestBody EmailRequest request) {
         return emailService.sendEmailWithToken(request, false, false);
     }
 
-    @PostMapping(path = "sendRegisterEmail")
+    @PostMapping(path = "/sendRegisterEmail")
     public ResponseEntity<?> sendRegisterEmail(@RequestBody EmailRequest request) {
         return emailService.sendEmailWithToken(request, true, false);
     }
 
-    @PostMapping(path = "sendPasswordResetEmail")
+    @PostMapping(path = "/sendPasswordResetEmail")
     public ResponseEntity<?> sendPasswordResetEmail(@RequestBody EmailRequest request) {
         return emailService.sendEmailWithToken(request, false, true);
     }
 
-    @GetMapping(path = "confirmToken")
+    @GetMapping(path = "/confirmToken")
     public ResponseEntity<?> confirmToken(@RequestParam("token") String token) {
         return emailService.confirmToken(token);
     }
