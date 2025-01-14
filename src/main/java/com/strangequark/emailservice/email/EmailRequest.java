07ea3462a0e4abfb5e2869655906408968d00918
StrangeQuark <zacharyzirkle@hotmail.com>
2025-01-14 17:00:19 -0600

    Remove Lombok

--- src/main/java/com/strangequark/emailservice/email/EmailRequest.java
+++ src/main/java/com/strangequark/emailservice/email/EmailRequest.java
@@ -1,17 +1,39 @@
 package com.strangequark.emailservice.email;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class EmailRequest {
     private String recipient;
     private String sender;
     private String email;
     private String subject;
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
