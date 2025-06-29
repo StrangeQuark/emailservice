fb8b8ee3c2c697624f5588898d65b0fbb634503b
StrangeQuark <zacharyzirkle@hotmail.com>
2025-06-29 13:50:32 -0500

    Change localhost call to match correct port

--- src/main/java/com/strangequark/emailservice/email/EmailService.java
+++ src/main/java/com/strangequark/emailservice/email/EmailService.java
@@ -101,8 +101,8 @@ public class EmailService implements EmailSender {
             //Send the email
             ResponseEntity response = send(request.getRecipient(),
                     request.getSender(),
-                    isRegister ? buildUserSignupEmail("http://localhost:3001/confirm-email?token=" + token) :
-                            isPasswordReset ? buildPasswordResetEmail("http://localhost:3001/new-password?token=" + token) : request.getEmail(),
+                    isRegister ? buildUserSignupEmail("http://localhost:6000/confirm-email?token=" + token) :
+                            isPasswordReset ? buildPasswordResetEmail("http://localhost:6000/new-password?token=" + token) : request.getEmail(),
                     request.getSubject());
             if(response.getStatusCodeValue() != 200) {
                 LOGGER.error(response.toString());
