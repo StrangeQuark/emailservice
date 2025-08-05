dca22ad5d02b47f36fb116920bf07e1c74927863
StrangeQuark <zacharyzirkle@hotmail.com>
2025-08-05 16:00:15 -0500

    Add getters and setters to ErrorResponse

--- src/main/java/com/strangequark/emailservice/response/ErrorResponse.java
+++ src/main/java/com/strangequark/emailservice/response/ErrorResponse.java
@@ -56,4 +56,24 @@ public class ErrorResponse {
         this(message);
         this.errorCode = errorCode;
     }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
 }
