ed7408f736a535d0a8917c57d69a99aacb77b28c
StrangeQuark <zacharyzirkle@hotmail.com>
2025-12-17 21:01:02 -0600

    Add constructors/getters/setters for EmailTemplate

--- src/main/java/com/strangequark/emailservice/template/EmailTemplate.java
+++ src/main/java/com/strangequark/emailservice/template/EmailTemplate.java
@@ -29,7 +29,66 @@ public class EmailTemplate {
     @Convert(converter = LocalDateTimeEncryptDecryptConverter.class)
     private LocalDateTime createdAt;
 
-    @Column(nullable=false)
     @Convert(converter = LocalDateTimeEncryptDecryptConverter.class)
     private LocalDateTime updatedAt;
+
+    public EmailTemplate() {
+
+    }
+
+    public EmailTemplate(String name, String subject, String body) {
+        this.name = name;
+        this.subject = subject;
+        this.body = body;
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
