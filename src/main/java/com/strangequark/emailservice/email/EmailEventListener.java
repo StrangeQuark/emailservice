8eb830c12d53b4ff63f32581e255fc292d28478f
StrangeQuark <zacharyzirkle@hotmail.com>
2025-12-20 18:56:25 -0600

    Remove commented code

--- src/main/java/com/strangequark/emailservice/email/EmailEventListener.java
+++ src/main/java/com/strangequark/emailservice/email/EmailEventListener.java
@@ -3,26 +3,17 @@ package com.strangequark.emailservice.email;
 import com.strangequark.emailservice.template.EmailTemplateRequest;
 import com.strangequark.emailservice.utility.JwtUtility; // Integration line: Auth
 import com.strangequark.emailservice.utility.TelemetryUtility; // Integration line: Telemetry
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.header.Header; // Integration line: Auth
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired; // Integration line: Telemetry
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.mock.web.MockHttpServletRequest; // Integration line: Auth
 import org.springframework.stereotype.Service;
-import org.springframework.util.backoff.FixedBackOff;
 import org.springframework.web.context.request.RequestContextHolder; // Integration line: Auth
 import org.springframework.web.context.request.ServletRequestAttributes; // Integration line: Auth
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Map; // Integration line: Telemetry
 
 @Service
@@ -42,31 +33,6 @@ public class EmailEventListener {
     public EmailEventListener(EmailService emailService) {
         this.emailService = emailService;
     }
-
-    // Initialize the email-events topics on startup
-//    @Bean
-//    public Collection<NewTopic> kafkaTopics() {
-//        return List.of(
-//                TopicBuilder.name("general-email-events").partitions(1).replicas(1).build(),
-//                TopicBuilder.name("template-email-events").partitions(1).replicas(1).build()
-//        );
-//    }
-
-//    @Bean
-//    public ConcurrentKafkaListenerContainerFactory<String, EmailRequest> generalFactory(ConsumerFactory<Object, Object> consumerFactory) {
-//        ConcurrentKafkaListenerContainerFactory<String, EmailRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
-//        factory.setConsumerFactory(consumerFactory);
-//        factory.setCommonErrorHandler(new DefaultErrorHandler(new FixedBackOff(1000L, 2)));
-//        return factory;
-//    }
-//
-//    @Bean
-//    public ConcurrentKafkaListenerContainerFactory<String, EmailTemplateRequest> templateFactory(ConsumerFactory<Object, Object> consumerFactory) {
-//        ConcurrentKafkaListenerContainerFactory<String, EmailTemplateRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
-//        factory.setConsumerFactory(consumerFactory);
-//        factory.setCommonErrorHandler(new DefaultErrorHandler(new FixedBackOff(1000L, 2)));
-//        return factory;
-//    }
 
     @KafkaListener(topics = "general-email-events", groupId = "email-group", containerFactory = "generalFactory")
     public void generalEmailEvents(ConsumerRecord<String, EmailRequest> record) {
